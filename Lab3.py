a3b0bad365296b2fe78e02750ff94da54c92bd38
CR1502 <craig.lionel2020@vitbhopal.ac.in>
2023-05-13 13:51:29 +0530

    updates

--- Lab3.py
+++ Lab3.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 # Read the dataset into a pandas dataframe
-data = pd.read_csv('lab3.csv')
-
+data = pd.read_csv('tv_shows.csv')
+print(data)
 # Scale numerical data using MinMaxScaler
 scaler = MinMaxScaler()
 scaled_data = scaler.fit_transform(data)
@@ -12,4 +12,4 @@ scaled_data = scaler.fit_transform(data)
 clean_data = pd.DataFrame(scaled_data, columns=data.columns)
 
 # Save the cleaned data to a new CSV file
-clean_data.to_csv('cleaned_lab3.csv', index=False)
+#clean_data.to_csv('cleaned_lab3.csv', index=False)
